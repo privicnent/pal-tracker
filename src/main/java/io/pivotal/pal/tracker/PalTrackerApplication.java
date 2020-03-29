07b751847ef8a9156499f1fda9e7b6d135e2dab2
“Vincent <Vincent.paulraj@rabobank.nl>
2020-03-29 20:41:03 +0530

    data source added

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -1,16 +1,22 @@
 package io.pivotal.pal.tracker;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class PalTrackerApplication {
 
     @Bean
-    TimeEntryRepository timeEntryRepository() {
-        return new InMemoryTimeEntryRepository();
+    TimeEntryRepository timeEntryRepository(DataSource mysqlDataSource) {
+
+
+        return new JdbcTimeEntryRepository(mysqlDataSource);
     }
+
 
     public static void main(String[] args) {
         SpringApplication.run(PalTrackerApplication.class, args);

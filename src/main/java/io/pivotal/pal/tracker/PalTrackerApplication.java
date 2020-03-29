f324c23d8a5bd16b1e9fee73d03dc7308295b4f6
“Vincent <Vincent.paulraj@rabobank.nl>
2020-03-29 21:08:14 +0530

    headlth check

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -12,8 +12,6 @@ public class PalTrackerApplication {
 
     @Bean
     TimeEntryRepository timeEntryRepository(DataSource mysqlDataSource) {
-
-
         return new JdbcTimeEntryRepository(mysqlDataSource);
     }
 

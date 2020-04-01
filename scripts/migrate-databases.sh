c97a4266edf4ffb25ce395f131eb41130df6f41b
“Vincent <Vincent.paulraj@rabobank.nl>
2020-04-01 18:20:04 +0530

    headlth check

--- scripts/migrate-databases.sh
+++ scripts/migrate-databases.sh
@@ -3,7 +3,7 @@ set -e
 
 app_name="${1:-pal-tracker}"
 script_dir="${2:-.}"
-service_name="${3:-tracker-database}"
+service_name="${3:-tracker-database1}"
 service_key="${4:-flyway-migration-key}"
 
 function pre_tunnel_exit() {
